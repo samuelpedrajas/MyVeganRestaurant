a3655ca84ce56dd1610f7f51a952522256fda3de
Samuel P <samuel.p991@gmail.com>
2020-12-13 18:34:05 +0100

    food sources

--- Scripts/ClickableArea.gd
+++ Scripts/ClickableArea.gd
@@ -10,13 +10,26 @@ var input_disabled = false
 func _on_ClickableArea_input_event(_viewport, _event, _shape_idx):
 	if Input.is_action_just_pressed("ui_click") and not self.input_disabled:
 		emit_signal("clicked")
-	elif Input.is_action_just_released("ui_click"):
+		_block_temporary()
+	elif Input.is_action_just_released("ui_click") and not self.input_disabled:
 		emit_signal("released")
+		_block_temporary()
 
 
 func enable():
 	self.input_disabled = false
+	$Timer.set_paused(false)
 
 
 func disable():
 	self.input_disabled = true
+	$Timer.set_paused(true)
+
+
+func _block_temporary():
+	self.input_disabled = true
+	$Timer.start()
+
+
+func _on_Timer_timeout():
+	self.input_disabled = false

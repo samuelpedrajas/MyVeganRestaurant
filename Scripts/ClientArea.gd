27ed3b301fbecf9955cbca1dd70de25f2f8af921
Samuel P <samuel.p991@gmail.com>
2020-12-28 00:32:25 +0100

    timeout seconds progress

--- Scripts/ClientArea.gd
+++ Scripts/ClientArea.gd
@@ -50,6 +50,8 @@ func _prepare_game():
 	for _i in range(0, average_client_number):
 		orders.append([])
 	guaranteed_coins = 0
+
+	# dish distribution
 	while guaranteed_coins < calculated_goal:
 		var categories = menu.get_children()
 		var n_categories = menu.get_child_count()
@@ -74,6 +76,8 @@ func _prepare_game():
 				guaranteed_coins += dish_obj["dish"].profit
 			if guaranteed_coins >= calculated_goal:
 				break
+
+	# sort and assign to order_lists
 	for i in range(0, average_client_number):
 		if orders[i] == []:
 			continue
@@ -82,16 +86,57 @@ func _prepare_game():
 		for _order in orders[i]:
 			order_lists[-1].append(_order["dish"])
 
+	# build timeout_seconds
+	average_time_for_client = usable_time / order_lists.size()
+	timeout_seconds = [0]
+	for _i in range(1, order_lists.size()):
+		timeout_seconds.append(average_time_for_client)
+
+	var num_added_var = floor(added_variability_percentage * order_lists.size())
+	if int(num_added_var) % 2 != 0:
+		num_added_var -= 1
+
+	var addition = 0
+	var var_limit = min(order_lists.size() - 1, num_added_var + 1)
+	# first is time 0
+	for i in range(1, var_limit):
+		if i % 2 != 0:
+			addition = rng.randf_range(base_variability, added_variability)
+			timeout_seconds[i] += addition
+		else:
+			timeout_seconds[i] -= addition
+
+	var num_var = order_lists.size()
+	if int(num_var - var_limit) % 2 != 0:
+		num_var -= 1
+
+	for i in range(var_limit, num_var):
+		if i % 2 != 0:
+			addition = rng.randf_range(0, base_variability)
+			timeout_seconds[i] += addition
+		else:
+			timeout_seconds[i] -= addition
+
+	# accumulate timeout_seconds
+	print(timeout_seconds)
+
 	print("CALCULATED GOAL: %s" % [str(calculated_goal)])
 	print("GUARANTEED COINS: %s" % [str(guaranteed_coins)])
 	print("CALCULATED AVERAGE CLIENT NUMBER: %s" % [str(average_client_number)])
+	print("AVG. TIME FOR CLIENT: %s" % [str(average_time_for_client)])
 	print("FINAL ORDER LIST NUMBER: %s" % [str(order_lists.size())])
 	print("FINAL ORDER LISTS:")
 	for _orders in order_lists:
 		var references = []
+		var total = 0.0
 		for _order in _orders:
-			references.append(_order.reference)
-		print(references)
+			total += _order.profit
+			references.append(
+				"%s = %s" % [
+					_order.reference, str(_order.profit)
+				]
+			)
+		print("Total: %s, References: %s" % [str(total), str(references)])
 
 
 func new_client():

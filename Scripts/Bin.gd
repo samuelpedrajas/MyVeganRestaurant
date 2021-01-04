dc88b28b33de2d8d55cf62997a374d9e54178964
Samuel P <samuel.p991@gmail.com>
2021-01-05 00:05:17 +0100

    double click

--- Scripts/Bin.gd
+++ Scripts/Bin.gd
@@ -1,56 +1,42 @@
 extends Node2D
 
-var throw_time1 = 0.4
-var throw_scale1 = Vector2(0.75, 0.75)
-var throw_scale2 = Vector2(0.5, 0.5)
+var throw_time = 0.4
+var throw_scale = Vector2(0.5, 0.5)
 
 
 func throw_item(item, origin):
 	var tween = Tween.new()
+	var timer = Timer.new()
 	tween.connect(
-		"tween_all_completed", self, "_tween_completed1",
+		"tween_all_completed", self, "_tween_completed",
 		[item, tween]
 	)
 	tween.interpolate_method(
 		item, "set_global_position", origin.get_throw_position(),
-		to_global($Origin.get_position()), throw_time1,
+		to_global($Destination.get_position()), throw_time,
 		Tween.TRANS_CUBIC, Tween.EASE_OUT
 	)
 	tween.interpolate_method(
-		item, "set_scale", item.get_scale(), throw_scale1,
-		throw_time1, Tween.TRANS_CUBIC, Tween.EASE_OUT
+		item, "set_scale", item.get_scale(), throw_scale,
+		throw_time, Tween.TRANS_CUBIC, Tween.EASE_OUT
 	)
+	timer.one_shot = true
+	timer.set_wait_time(throw_time - 0.2)
+	timer.connect("timeout", self, "_item_arriving", [timer])
 	add_child(tween)
-	add_child(item)
+	add_child(timer)
 	tween.start()
+	timer.start()
 
 
-func _tween_completed1(item, tween):
-	var throw_time2 = $AnimationPlayer.get_animation("throw").length
+func _item_arriving(timer):
 	$AnimationPlayer.play("throw")
-	tween.disconnect(
-		"tween_all_completed", self, "_tween_completed1"
-	)
-	tween.connect(
-		"tween_all_completed", self, "_tween_completed2",
-		[item, tween]
-	)
-	tween.interpolate_method(
-		item, "set_position", $Origin.get_position(),
-		$Destination.get_position(), throw_time2
-	)
-	tween.interpolate_property(
-		item, "modulate", item.modulate,
-		item.modulate * Color(1.0, 1.0, 1.0, 0.0), throw_time2
-	)
-	tween.interpolate_method(
-		item, "set_scale", item.get_scale(), throw_scale2,
-		throw_time2, Tween.TRANS_CUBIC, Tween.EASE_OUT
-	)
-	tween.start()
+	remove_child(timer)
+	timer.queue_free()
 
 
-func _tween_completed2(item, tween):
+func _tween_completed(item, tween):
 	remove_child(tween)
 	tween.queue_free()
+	item.get_parent().remove_child(item)
 	item.queue_free()
--- Scripts/Kitchen.gd
+++ Scripts/Kitchen.gd
@@ -168,6 +168,7 @@ func throw_to_bin(item, origin):
 	print("%s points losed" % item.discard_price)
 	substract_score(item.discard_price)
 	origin.drop_item()
+	$Main.add_child(item)
 	$Main/Bin.throw_item(item, origin)
 
 

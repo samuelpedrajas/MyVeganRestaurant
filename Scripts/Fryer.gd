1a2c77d46256bfc19fe78ff1c253009ee56b93de
Samuel P <samuel.p991@gmail.com>
2021-01-09 21:18:56 +0100

    fryer improved

--- Scripts/Fryer.gd
+++ Scripts/Fryer.gd
@@ -8,10 +8,6 @@ var ingredient = null
 var upgrade = 0
 
 
-func _ready():
-	$AnimationPlayer.play("default_animation")
-
-
 func is_busy():
 	return self.ingredient != null or self.platform.delivery != null
 
@@ -21,20 +17,32 @@ func add_item(_ingredient):
 		print("ERROR: machine is busy!!!")
 		return
 	self.ingredient = _ingredient
-	$AnimationPlayer.play("preparing_animation")
+	_play_preparing_animation()
 	$Timer.start()
 
 
 func send_item():
 	platform.add_ingredient(self.ingredient)
 	self.ingredient = null
-	$AnimationPlayer.play("default_animation")
+	_play_default_animation()
 	$Timer.stop()
 
 
 func set_config(machine_level, machine_upgrades):
 	self.upgrade = machine_level
-	print("Fryer upgraded")
+	_play_default_animation()
+	$Timer.cooking_time = machine_upgrades["CookingTime"]
+	print("%s upgraded" % [get_name()])
+
+
+func _play_default_animation():
+	var anim_name = "default_animation_%s" % [str(self.upgrade)]
+	$AnimationPlayer.play(anim_name)
+
+
+func _play_preparing_animation():
+	var anim_name = "preparing_animation_%s" % [str(self.upgrade)]
+	$AnimationPlayer.play(anim_name)
 
 
 func _on_Timer_food_cooked():
